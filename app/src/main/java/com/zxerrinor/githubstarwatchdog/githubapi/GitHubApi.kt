e708da563597d0dc1014e0a9cf9726c15bfa92ce
zxerrinor <zxerrinor@gmail.com>
2021-02-18 15:45:58 +0300

    REST API client for GitHub

---
+++ app/src/main/java/com/zxerrinor/githubstarwatchdog/githubapi/GitHubApi.kt
@@ -0,0 +1,47 @@
+package com.zxerrinor.githubstarwatchdog.githubapi
+
+import com.zxerrinor.githubstarwatchdog.CurrentValuesStore.gitHubAuthToken
+import retrofit2.Call
+import retrofit2.http.*
+
+interface GitHubApi {
+    @GET("/repos/{user}/{repo}/stargazers")
+    @Headers("Accept: application/vnd.github.v3.star+json")
+    fun getStargazers(
+        @Path("user") userName: String,
+        @Path("repo") repoName: String,
+        @Query("page") page: Int,
+        @Query("per_page") resultsInPage: Int,
+        @Header("Authorization") authToken: String = "token $gitHubAuthToken"
+    ): Call<List<Stargazer>>
+
+    @GET("/repos/{user}/{repo}")
+    @Headers("Accept: application/vnd.github.v3.star+json")
+    fun getRepositoryInfo(
+        @Path("user") userName: String,
+        @Path("repo") repoName: String,
+        @Header("Authorization") authToken: String = "token $gitHubAuthToken"
+    ): Call<Repository>
+
+    @GET("/users/{user}/repos")
+    @Headers("Accept: application/vnd.github.v3.star+json")
+    fun getRepositoriesOfUser(
+        @Path("user") userName: String,
+        @Query("page") page: Int,
+        @Query("per_page") resultsInPage: Int,
+        @Header("Authorization") authToken: String = "token $gitHubAuthToken"
+    ): Call<List<Repository>>
+
+    @GET("/users/{user}")
+    @Headers("Accept: application/vnd.github.v3.star+json")
+    fun getUser(
+        @Path("user") userName: String,
+        @Header("Authorization") authToken: String = "token $gitHubAuthToken"
+    ): Call<User>
+
+    @GET("/rate_limit")
+    @Headers("Accept: application/vnd.github.v3.star+json")
+    fun getRateLimit(
+        @Header("Authorization") authToken: String = "token $gitHubAuthToken"
+    ): Call<RateLimit>
+}
