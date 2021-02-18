ba4398b3f8913f2edf32a51671bc7ee6f49d5a38
zxerrinor <zxerrinor@gmail.com>
2021-02-18 16:06:24 +0300

    Added delete cache action

--- app/src/main/java/com/zxerrinor/githubstarwatchdog/ui/MainActivity.kt
+++ app/src/main/java/com/zxerrinor/githubstarwatchdog/ui/MainActivity.kt
@@ -1,23 +1,19 @@
-package com.zxerrinor.githubstarwatchdog
+package com.zxerrinor.githubstarwatchdog.ui
 
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import com.zxerrinor.githubstarwatchdog.CurrentValuesStore
+import com.zxerrinor.githubstarwatchdog.R
+import com.zxerrinor.githubstarwatchdog.clearDatabaseCache
+import moxy.MvpAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : MvpAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        CurrentValuesStore.activity = this
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,7 +27,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_clear_cache -> {
+                clearDatabaseCache()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
