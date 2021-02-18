94078d38b4ed9f60fd5f59d83a68c8d4b19f21a9
zxerrinor <zxerrinor@gmail.com>
2021-02-18 16:09:15 +0300

    Background worker

---
+++ app/src/main/java/com/zxerrinor/githubstarwatchdog/LoadDataForFavouriteRepositoriesWorker.kt
@@ -0,0 +1,47 @@
+package com.zxerrinor.githubstarwatchdog
+
+import android.R
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.zxerrinor.githubstarwatchdog.ui.MainActivity
+
+
+class LoadDataForFavouriteRepositoriesWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
+    override fun doWork(): Result {
+        if (!isInternetAvailable()) return Result.retry()
+        val rateLimitResponse =
+            (App.gitHubApi.getRateLimit().execute().body() ?: return Result.retry())
+        if (rateLimitResponse.rate.remaining < 10) return Result.retry()
+
+        App.db.repositoryDao().getAllByFavouriteState(true).forEach {
+            loadAndSaveRepoStargazers(it.repoUserName, it.repoName)
+        }
+
+        val intent = Intent(this.applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this.applicationContext, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this.applicationContext, "mainChannel")
+            .setSmallIcon(R.drawable.sym_def_app_icon)
+            .setContentTitle("GitHub Stars Updated")
+            .setContentText("Successfully updated stars of your favourite repositories!")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager =
+            this.applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
+        notificationManager!!.notify(1, builder.build())
+
+        return Result.success()
+    }
+}
