efa0857c05ac52e38e80c1c47324e8ca2df68a8e
Anton Potapov <anton.potapov@omega-r.com>
2021-03-25 11:17:41 +0300

    Add constant names for arguments of fragments.

--- app/src/main/java/com/zxerrinor/githubstarwatchdog/App.kt
+++ app/src/main/java/com/zxerrinor/githubstarwatchdog/App.kt
@@ -12,6 +12,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
+const val MONTH_ARGUMENT_NAME = "month"
+const val MONTH_NUMBERS_ARGUMENT_NAME = "monthNumbers"
+const val REPO_NAME_ARGUMENT_NAME = "repoName"
+const val REPO_USER_NAME_ARGUMENT_NAME = "repoUserName"
+const val OFFLINE_MODE_ARGUMENT_NAME = "offlineMode"
+const val GITHUB_AUTH_TOKEN = "dd2937208d893eca075a22dddeeacf95ab419e29"
+
 class App : Application() {
     private lateinit var retrofit: Retrofit
     override fun onCreate() {
